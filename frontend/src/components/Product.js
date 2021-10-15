f3be346d3c92102336591e40e0d40b9068527ab8
Makhmut <mahmudlyder@gmail.com>
2021-10-15 22:32:57 +0600

    product and home pages

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -6,26 +6,19 @@ export default function Product(props) {
     const {product}=props;
     return (
         <div key={product._id} className="card">
-            <Link to={`/product/${product._id}`}>
-                <img className="medium" src={product.image} alt={product.name} />
-            </Link>
-            <div className="card-body">
-                <Link to={`/product/${product._id}`}>
-                <h2>{product.name}</h2>
-                </Link>
-                <Rating
-                rating={product.rating}
-                numReviews={product.numReviews}
+            <a href={`/product/${product._id}`}>
+                <img class="medium" src={product.image} alt={product.name} />
+              </a>
+              <div class="card-body">
+              <a href={`/product/${product._id}`}>
+                  <h2>{product.name}</h2>
+                </a>
+                <Rating 
+                    rating={product.rating}
+                    numReviews={product.numReviews}
                 ></Rating>
-                <div className="row">
-                <div className="price">${product.price}</div>
-                <div>
-                    <Link to={`/seller/${product.seller._id}`}>
-                    {product.seller.seller.name}
-                    </Link>
-                </div>
-                </div>
-            </div>
+                <div class="price">${product.price}</div>
+              </div>
         </div>
     )
 }
